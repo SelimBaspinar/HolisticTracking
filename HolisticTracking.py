9105f81c4da9d80bf4f9b80c83742c8704bbb7a4
Selim Başpınar <56540379+SelimBaspinar@users.noreply.github.com>
2022-09-26 17:29:13 +0300

    Add files via upload

--- HolisticTracking.py
+++ HolisticTracking.py
@@ -5,32 +5,26 @@ import time
 
 class handDetector():
     static_image_mode = False,
-    max_num_hands = 2,
     model_complexity = 1,
     min_detection_confidence = 0.5,
     min_tracking_confidence = 0.5
-    def __init__(self, mode=False, maxHands=2,complexity=1, detectionCon=0.5, trackCon=0.5):
+    def __init__(self, mode=False,complexity=1, detectionCon=0.5, trackCon=0.5):
         self.mode = mode
         self.complexity = complexity
-        self.maxHands = maxHands
         self.detectionCon = detectionCon
         self.trackCon = trackCon
 
-        self.mpHands = mp.solutions.holistic
-        self.hands = self.mpHands.Holistic()
+        self.mpHolistic = mp.solutions.holistic
+        self.pose = self.mpHolistic.Holistic()
         self.mpDraw = mp.solutions.drawing_utils
 
-    def findHands(self, img, draw=True):
+    def findPose(self, img, draw=True):
         imgRGB = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
-        self.results = self.hands.process(imgRGB)
+        self.results = self.pose.process(imgRGB)
         if draw:
-            self.mpDraw.draw_landmarks(img,self.results.right_hand_landmarks , self.mpHands.HAND_CONNECTIONS)
-            self.mpDraw.draw_landmarks(img,self.results.left_hand_landmarks , self.mpHands.HAND_CONNECTIONS)
-            for hands in self.results.left_hand_landmarks:
-                for id, lm in enumerate(hands.landmark):
-                    print(id, lm)
-                    h, w, c = img.shape
-                    cx, cy = int(lm.x * w), int(lm.y * h)
+            self.mpDraw.draw_landmarks(img,self.results.face_landmarks , self.mpHolistic.POSE_CONNECTIONS)
+            self.mpDraw.draw_landmarks(img,self.results.pose_landmarks , self.mpHolistic.POSE_CONNECTIONS)
+
         return img
 
     def findPosition(self, img, handNo=0, draw=True):
@@ -55,7 +49,7 @@ def main():
 
     while True:
         success, img = cap.read()
-        img = detector.findHands(img)
+        img = detector.findPose(img)
         # lmlist=detector.findPosition(img)
         # if(len(lmlist)!=0):
         #     print(lmlist[4])
